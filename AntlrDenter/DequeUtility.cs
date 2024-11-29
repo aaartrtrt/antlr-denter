3197a412f971b50caaa63fdd19433bbece625e51
art <aaartrtrt@github.com>
2024-11-29 22:39:21 +1030

    feat: Move Deque code onto repo.

---
+++ AntlrDenter/DequeUtility.cs
@@ -0,0 +1,88 @@
+/**
+ * Deque, as on https://github.com/tejacques/Deque.
+ * 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013 Tom Jacques
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DequeUtility
+{
+    internal class Utility
+    {
+        public static int ClosestPowerOfTwoGreaterThan(int x)
+        {
+            x--;
+            x |= (x >> 1);
+            x |= (x >> 2);
+            x |= (x >> 4);
+            x |= (x >> 8);
+            x |= (x >> 16);
+            return (x+1);
+        }
+
+        /// <summary>
+        /// Jon Skeet's excellent reimplementation of LINQ Count.
+        /// </summary>
+        /// <typeparam name="TSource">The source type.</typeparam>
+        /// <param name="source">The source IEnumerable.</param>
+        /// <returns>The number of items in the source.</returns>
+        public static int Count<TSource>(IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            // Optimization for ICollection<T> 
+            ICollection<TSource> genericCollection = source as ICollection<TSource>;
+            if (genericCollection != null)
+            {
+                return genericCollection.Count;
+            }
+
+            // Optimization for ICollection 
+            ICollection nonGenericCollection = source as ICollection;
+            if (nonGenericCollection != null)
+            {
+                return nonGenericCollection.Count;
+            }
+
+            // Do it the slow way - and make sure we overflow appropriately 
+            checked
+            {
+                int count = 0;
+                using (var iterator = source.GetEnumerator())
+                {
+                    while (iterator.MoveNext())
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
+}
