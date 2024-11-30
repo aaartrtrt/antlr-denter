70feadc304db68f2c6db9970777f398d7845d054
art <aaartrtrt@github.com>
2024-11-30 22:11:02 +1030

    feat: Start making an example project based on one of the grammars.

--- AntlrDenter/DenterOptions.cs
+++ AntlrDenter/DenterOptions.cs
@@ -1,13 +1,39 @@
+/**
+ * AntlrDenter, as on https://github.com/yshavit/antlr-denter.
+ * 
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Yuval Shavit:
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace AntlrDenter
 {
     public interface IDenterOptions {
         /**
-   * Don't do any special handling for EOFs; they'll just be passed through normally. That is, we won't unwind indents
-   * or add an extra NL.
-   *
-   * This is useful when the lexer will be used to parse rules that are within a line, such as expressions. One use
-   * case for that might be unit tests that want to exercise these sort of "line fragments".
-   */
+         * Don't do any special handling for EOFs; they'll just be passed through normally. That is, we won't unwind indents
+         * or add an extra NL.
+         *
+         * This is useful when the lexer will be used to parse rules that are within a line, such as expressions. One use
+         * case for that might be unit tests that want to exercise these sort of "line fragments".
+         */
         void IgnoreEof();
     }
 }
---
+++ SimpleCalc/SimpleCalc.g4
@@ -0,0 +1,34 @@
+grammar SimpleCalc;
+
+tokens { INDENT, DEDENT }
+@lexer::header {
+  using AntlrDenter;
+}
+@lexer::members {
+  private DenterHelper denter;
+    
+  public override IToken NextToken()
+  {
+      if (denter == null)
+      {
+          denter = DenterHelper.Builder()
+              .Nl(NL)
+              .Indent(SimpleCalcLexer.INDENT)
+              .Dedent(SimpleCalcLexer.DEDENT)
+              .PullToken(base.NextToken);
+      }
+
+      return denter.NextToken();
+  }
+}
+
+expr: OP INDENT expr expr DEDENT # Operation
+    | INT NL                     # IntLiteral
+    ;
+
+NL: ('\r'? '\n' ' '*); // note the ' '*
+WS: [ \t]+ -> skip;
+LINE_COMMENT: '//' ~[\r\n]* -> skip;
+
+INT: [0-9]+;
+OP: [a-zA-Z]+;
